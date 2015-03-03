08357f8b39b22e622e383df8448668ba3746765b
Patrick Greenwell <greenwell.patrick@gmail.com>
2015-03-03 15:46:35 -0500

    Initial Project

---
+++ Services3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Owin.Hosting;
+
+namespace Services3
+{
+	class Program
+	{
+		private static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
+		static void Main(string[] args)
+		{
+			int port = 5000;
+			if(args.Length > 0)
+			{
+				int.TryParse(args[0], out port);
+			}
+
+			Console.CancelKeyPress += (sender,eArgs) => 
+			{
+						 _quitEvent.Set();
+						 eArgs.Cancel = true;
+			};
+			//We'll listen for all HTTP websites on the given port.
+			var siteURI = string.Format("http://*:{0}",port);
+			
+			
+			using(WebApp.Start<Startup>(siteURI))
+			{ 
+				Console.WriteLine("Started");
+				_quitEvent.WaitOne();
+			}
+
+		}
+	}
+}
---
+++ Services3/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace Services3
+{
+	public class Startup
+	{
+		public void Configure(IAppBuilder app)
+		{
+			app.UseWelcomePage();
+		}
+	}
+}
